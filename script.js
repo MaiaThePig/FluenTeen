c2434ae6bc296f8ca78dd450564a9f785eac53e1
MaiaThePig <52360131+MaiaThePig@users.noreply.github.com>
2023-10-30 11:05:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,18 @@ document.addEventListener('DOMContentLoaded', function() {
         resultContainer.style.display = 'block';
     }
     });
+function openModal(modal)
+{
+  var modal = document.getElementById(modal);
+    modal.style.display = "block";
+    var span = document.querySelector("#"+modal.id+" .close");
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+});
